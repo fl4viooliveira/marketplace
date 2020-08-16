06df1953c7b46d9bba8743024844726ba84e4c60
fl4viooliveira <fl4viooliveira@gmail.com>
2020-08-16 13:44:22 +0100

    book list view

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Author, Book, Chapter, Exercise, Solution
+
+admin.site.register(Author)
+admin.site.register(Book)
+admin.site.register(Chapter)
+admin.site.register(Exercise)
+admin.site.register(Solution)
 
 # Register your models here.
--- digital_marketplace/settings.py
+++ digital_marketplace/settings.py
@@ -1,4 +1,4 @@
-import os
+# import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -44,7 +44,8 @@ ROOT_URLCONF = 'digital_marketplace.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        # 'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -107,10 +108,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+# STATIC_URL = '/static/'
+# MEDIA_URL = '/media/'
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static')
+# ]
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
+
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
+
+STATIC_ROOT = BASE_DIR / 'static' # is equal to os.path.join(BASE_DIR, 'static/')
+MEDIA_ROOT = BASE_DIR / 'media' # is equal to os.path.join(BASE_DIR, 'media/')
