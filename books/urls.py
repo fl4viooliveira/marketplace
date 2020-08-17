2104187dc4717cc49293e312759091734144efba
fl4viooliveira <fl4viooliveira@gmail.com>
2020-08-17 22:32:33 +0100

    book_detail, chapter_detail templates

--- books/urls.py
+++ books/urls.py
@@ -11,7 +11,7 @@ app_name = 'books'
 urlpatterns = [
     path('', book_list, name='book-list'),
     path('<slug>/', book_detail, name='book-detail'),
-    path('<book_slug>/<chapter_number>', 
+    path('<book_slug>/<chapter_number>/', 
     chapter_detail,
     name='chapter-detail'),
     path('<book_slug>/<chapter_number>/<exercise_number>/', 
