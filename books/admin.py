6f1eb54ea88d30df0bf4c0f155b90b21b607d019
fl4viooliveira <fl4viooliveira@gmail.com>
2020-08-22 12:56:19 +0100

    user library

--- books/admin.py
+++ books/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
-from .models import Author, Book, Chapter, Exercise, Solution
+from .models import Author, Book, Chapter, Exercise, Solution, UserLibrary
 
 admin.site.register(Author)
 admin.site.register(Book)
 admin.site.register(Chapter)
 admin.site.register(Exercise)
 admin.site.register(Solution)
+admin.site.register(UserLibrary)
 
 # Register your models here.
