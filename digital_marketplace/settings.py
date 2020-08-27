ab71ce96593e087462a525a09e262970e8a7e69f
fl4viooliveira <fl4viooliveira@gmail.com>
2020-08-27 12:20:32 +0100

    profile_view

--- digital_marketplace/settings.py
+++ digital_marketplace/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
 
     # own apps
     'books',
+    'core',
     'shopping_cart',
 ]
 
--- digital_marketplace/urls.py
+++ digital_marketplace/urls.py
@@ -1,16 +1,21 @@
+
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+from core.views import profile_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
+    path('account/profile/', profile_view, name='profile'),
     path('cart/', include('shopping_cart.urls', namespace='cart')),
     path('', include('books.urls', namespace='books'))
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
